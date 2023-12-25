93e4eb078626f80640c289e0c3a7fff16650505d
Rafikk98 <rafikk1998@gmail.com>
2023-12-26 02:46:54 +0600

    Solved Merge Strings Alternatively

---
+++ Array-String/GreatestCommonDivisorofStrings.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+const gcdOfStrings = function(str1, str2) {
+    
+};
---
+++ Array-String/MergeStringsAlternately.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+const mergeStringsAlternately = function(word1, word2) {
+    let result = '';
+    const [m, n] = [word1.length, word2.length];
+    for (let i = 0, j = 0; i < m || j < n;) {
+        if (i < m) {
+            result += word1[i++];
+        }
+        if (j < n) {
+            result += word2[j++];
+        }
+    }
+    return result;
+};
+
+console.log(mergeStringsAlternately("ab", "pqrs"));
