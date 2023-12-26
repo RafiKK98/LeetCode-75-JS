0795cff33ce1131914ebb7148b1f9f9ecde11d54
Rafikk98 <rafikk1998@gmail.com>
2023-12-27 01:49:53 +0600

    Solved Reverse words in a String problem

--- Array-String/06-Reverse-Words-in-a-String.js
+++ Array-String/06-Reverse-Words-in-a-String.js
@@ -3,7 +3,17 @@
  * @return {string}
  */
 const reverseWords = function(s) {
-    
+    const wordArray = s.trim().split(' ');
+    const reversedArray = [];
+    let reverseString = '';
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        if (wordArray[i] != '') {
+            // reversedArray.push(wordArray[i]);
+            reverseString += wordArray[i] + " ";
+        }
+    }
+    // return reversedArray.join(' ');
+    return reverseString + wordArray[0];
 };
 
 console.log(reverseWords("the sky is blue"));
